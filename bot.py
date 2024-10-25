06476ee8f21a786d1daa2e172b1b1fdf8a77f3ea
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-25 07:04:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="?")
 infostuff = "## Welcome to Starry, and open source OSNIT discord bot\n"
 @bot.command()
 async def info(ctx):
