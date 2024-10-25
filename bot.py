312890d9679c771e0bf5b3ba76e3894fbfb28ed7
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-25 07:33:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,21 @@ infostuff = "## Welcome to Starry, and open source OSNIT discord bot\n"
 async def info(ctx):
     await ctx.send(infostuff)
 @bot.command()
-async def ip(ctx):
-    ipy = [ip:4]
-    requests.post
+async def ip(ctx, ip_address):
+    response = requests.get(f"http://ip-api.com/json/{ip_address}")
+    if response.statuscode == 200:
+        data = response.json
+        message = (
+            f"**IP Address Information**\n"
+            f"IP: {data['query']}\n"
+            f"City: {data.get('city', 'N/A')}\n"
+            f"Region: {data.get('regionName', 'N/A')}\n"
+            f"Country: {data.get('country', 'N/A')}\n"
+            f"ISP: {data.get('isp', 'N/A')}\n"
+            f"Lat, Long: {data.get('lat', 'N/A')}, {data.get('lon', 'N/A')}"
+        )
+        await ctx.send(message)
+    else:
+        await ctx.send("An error has occurred")
 #end cmd
 bot.run("private_token")
