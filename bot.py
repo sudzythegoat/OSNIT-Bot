432397948116ede5d1d9af24aafd124da9c3210a
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-25 07:11:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
 import discord
 from discord.ext import commands
+import requests
 bot = commands.Bot(command_prefix="?")
 infostuff = "## Welcome to Starry, and open source OSNIT discord bot\n"
+#cmds
 @bot.command()
 async def info(ctx):
     await ctx.send(infostuff)
-
+@bot.command()
+async def ip(ctx):
+    ipy = [ip:4]
+    requests.post
+#end cmd
 bot.run("private_token")
