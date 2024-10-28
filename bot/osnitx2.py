12449cc89db29283bcee3ae4ceeef431ce63c6c3
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-27 21:04:28 -0500

    Update osnitx2.py

--- bot/osnitx2.py
+++ bot/osnitx2.py
@@ -1,3 +1,15 @@
 import requests
+import json
+import phonenumbers
 from phonenumbers import carrier
 from phonenumbers import geocoder
+from googlesearch import search
+class OsnitX:
+    @staticmethod
+    def phonelocation(number):
+        parsed = phonenumbers.parse(number, None)
+        location = geocoder.description_for_number(parsed, "en")
+        return location
+    @staticmethod
+    def iplocation(ip):
+        
